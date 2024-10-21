325377b095f0e64f682a3421e0745393cd3d573d
Dhruv <dhruvchaudhary1611@gmail.com>
2024-10-21 11:52:41 +0530

    Production Ready-5

--- client/src/lib/apiRequest.js
+++ client/src/lib/apiRequest.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiRequest = axios.create({
-  baseURL: "http://localhost:8800/api",
+  baseURL: "https://realar-2w37.onrender.com/api",
   withCredentials: true,
 });
 
